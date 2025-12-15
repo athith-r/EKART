17eec2b71b8592f6a825badfa9a129c8a82e63cd
athith-r <kit28.24bcb009@gmail.com>
2025-12-15 13:04:27 +0530

    Starting point of the project

---
+++ src/main/java/com/ecommerce/EcommerceApplication.java
@@ -0,0 +1,14 @@
+package com.ecommerce;
+import org.springframework.boot.SpringApplication; //Spring Boot import
+import org.springframework.boot.autoconfigure.SpringBootApplication; //Spring Boot annotation import
+
+@SpringBootApplication
+public class EcommerceApplication {
+    public static void main(String[] args) {  
+        SpringApplication.run(EcommerceApplication.class, args); //Run the Spring Boot applicatioN (It's the static main method)
+        //All the data sent to spring boot through EcommerceApplication class...
+        //Args is an array of string arguments passed to the application which helps in configuring the application at runtime.(Additional parameters)
+        //All spring boot applications start from the main method. 
+        
+    }
+}
